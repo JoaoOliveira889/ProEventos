011169a188c0cc3dab8f31356fad3020a042bbe7
João Paulo Oliveira <joaop.oliveira@havan.com.br>
2021-09-03 16:00:54 -0300

    Project Commit

---
+++ Server/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular 11 e .Net 5",
+                    Local = "Belo Horizonte",
+                    Lote = "1 Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+                    },
+                    new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular e novidades",
+                    Local = "São Paulo",
+                    Lote = "2 Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+                    }
+        };
+
+        public EventosController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(e => e.EventoId == id);
+        }
+    }
+}
